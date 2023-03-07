097b93be849fa979dea89c1d6f62c04853202a5b
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-03-07 20:52:35 +0300

    Header css yapildi

--- src/components.js/Header.js
+++ src/components.js/Header.js
@@ -3,12 +3,14 @@ import React from 'react';
 export default function Header() {
     const titleText = `Lorem ipsum dolor sit amet`;
     return (
-        <div className="App">
+        <div className="Header">
             <h1>Friends Database</h1>
-            <button>LOGIN.</button>
-            <button>LOGIN.</button>
-            <button>LOGIN.</button>
-            <button>LOGIN.</button>
+            <div>
+                <button>LOGIN.</button>
+                <button>FRIENLIST.</button>
+                <button>ADDFRIEND.</button>
+                <button>LOGOUT.</button>
+            </div>
         </div>
     )
 }
