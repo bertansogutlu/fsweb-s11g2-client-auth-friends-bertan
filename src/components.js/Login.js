352b4ded993219b2eadfa5d04e4702087a3785a4
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-03-07 22:52:47 +0300

    login icerik yazilari duzeltildi

--- src/components.js/Login.js
+++ src/components.js/Login.js
@@ -22,11 +22,11 @@ export default function Login() {
             <div className="Login">
                 <h2>LOGIN</h2>
                 <label>
-                    <h3>Username</h3>
+                    <h3>USERNAME</h3>
                     <input value={login.username} onChange={(event) => setLogin({ ...login, username: event.target.value })}></input>
                 </label>
                 <label>
-                    <h3>Password</h3>
+                    <h3>PASSWORD</h3>
                     <input value={login.password} onChange={(event) => setLogin({ ...login, password: event.target.value })}></input>
                 </label>
                 <button>
